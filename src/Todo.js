f800dfd3af7c21e5abd7bcc6e293c8d43785627e
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2020-01-22 03:04:36 +0200

    finishing the core funcitonality of the todolist before final style

--- src/Todo.js
+++ src/Todo.js
@@ -1,25 +1,69 @@
 import React, {Component} from 'react';
+import uuid from 'uuid';
+import './Todo.css';
 
 class Todo extends Component {
     constructor(props){
         super(props);
-        this.handleEdit = this.handleEdit.bind(this);
+        this.state = {
+        isEditing: false,
+        task: this.props.task
+        };
         this.handleRemove = this.handleRemove.bind(this);
-    }
-    handleEdit(evt){
-
+        this.toogleForm = this.toogleForm.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
     handleRemove(evt){
         this.props.removeTodo(this.props.id);
     }
+    toogleForm(){
+        this.setState({isEditing: !this.state.isEditing });
+    }
+    handleUpdate(evt) {
+        evt.preventDefault();
+        // tabke new task data and pass up to parent
+        this.props.updateTodo(this.props.id, this.state.task);
+        this.setState({isEditing: false });
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.createTodo({...this.state,id:uuid()});
+        this.setState({task: "" });
+    }
+    handleToggle(evt){
+        this.props.toggleTodo(this.props.id);
+    }
     render() {
-        return(
-            <div>
-                <button onClick={this.handleEdit}>Edit</button>
+        let result;
+        if(this.state.isEditing) {
+            result = (
+                <div>
+                <form onSubmit={this.handleUpdate}>
+                <input type="text" value={this.state.task} name="task" onChange={this.handleChange}/>
+                <button>Save</button>
+                </form>
+                </div>
+            )
+        }
+        else {
+            result = (
+                <div>
+                <button onClick={this.toogleForm}>Edit</button>
                 <button onClick={this.handleRemove}>X</button>
-                <li>{this.props.task}</li>
-            </div>
-        )
+                <li className={this.props.completed ? "completed" : ""} onClick={this.handleToggle}>{this.props.task}</li>
+                </div>
+            )
+        }
+
+        return result;
     }
 }
 export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,8 @@ class TodoList extends Component {
         this.state = {todos:[]}
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
+        this.toggleCompletion = this.toggleCompletion.bind(this);
     }
     create(newTodo) {
         this.setState({
@@ -19,9 +21,36 @@ class TodoList extends Component {
             todos: this.state.todos.filter(t => t.id !== id)
         })
     }
+    update(id,updatedTask){
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id) {
+                return {...todo, task: updatedTask};
+            }
+            return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    }
+    toggleCompletion(id) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id) {
+                return {...todo, completed: !todo.completed};
+            }
+            return todo;
+           
+        });
+        this.setState({ todos: updatedTodos });
+    }
     render() {
         const todos = this.state.todos.map(todo=> {
-            return <Todo key={todo.id} task={todo.task} id={todo.id} removeTodo={this.remove} />
+            return <Todo 
+            key={todo.id} 
+            task={todo.task} 
+            id={todo.id}
+            completed={todo.completed}
+            removeTodo={this.remove}
+            updateTodo={this.update} 
+            toggleTodo={this.toggleCompletion}
+            />
         })
         return(
             <div>
